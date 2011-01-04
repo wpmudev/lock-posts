5ffcfe567797b640ae46dfe9b4049e33dd688611
ulrich <ulrich@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-04 15:43:00 +0000

    Lock Posts 1.0.2
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/lock-posts@1818 f53fe46e-8c02-459b-9fdd-93e75771b278

--- lock-posts.php
+++ lock-posts.php
@@ -1,15 +1,15 @@
 <?php
 /*
 Plugin Name: Lock Posts
-Plugin URI: 
-Description:
-Author: Andrew Billits
-Version: 1.0.1
-Author URI:
-*/ 
+Plugin URI: http://premium.wpmudev.org/project/lock-posts
+Description: This plugin allows site admin to lock down posts on any blog so that regular ol' users just can't edit them - for example, with a school assignment - stop it from being edited after submission.
+Author: Andrew Billits, Ulrich Sossou
+Version: 1.0.2
+Author URI: http://premium.wpmudev.org/
+*/
 
-/* 
-Copyright 2007-2009 Incsub (http://incsub.com)
+/*
+Copyright 2007-2011 Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
@@ -25,119 +25,148 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-//------------------------------------------------------------------------//
-//---Config-----------------------------------------------------------------//
-//------------------------------------------------------------------------//
+class Lock_Posts {
 
-//------------------------------------------------------------------------//
-//---Hook-----------------------------------------------------------------//
-//------------------------------------------------------------------------//
+	/**
+	 * PHP4 constructor
+	 *
+	 */
+	function Lock_Posts() {
+		__construct();
+	}
 
-add_action('admin_menu', 'lock_posts_meta_box');
-add_action('save_post', 'lock_posts_update');
-add_action('init', 'lock_posts_check');
-add_action('manage_posts_custom_column', 'lock_posts_status_output', 2, 2);
-add_filter('manage_posts_columns', 'lock_posts_status_column');
-add_action('manage_pages_custom_column', 'lock_posts_status_output', 2, 2);
-add_filter('manage_pages_columns', 'lock_posts_status_column');
+	/**
+	 * PHP5 constructor
+	 *
+	 */
+	function __construct() {
+		add_action( 'admin_menu', array( &$this, 'meta_box' ) );
+		add_action( 'admin_menu', array( &$this, 'admin_page' ) );
+		add_action( 'save_post', array( &$this, 'update' ) );
+		add_action( 'init', array( &$this, 'check' ) );
+		add_action( 'manage_posts_custom_column', array( &$this, 'status_output' ), 2, 2 );
+		add_filter( 'manage_posts_columns', array( &$this, 'status_column' ) );
+		add_action( 'manage_pages_custom_column', array( &$this, 'status_output' ), 2, 2 );
+		add_filter( 'manage_pages_columns', array( &$this, 'status_column' ) );
+	}
 
-//------------------------------------------------------------------------//
-//---Functions------------------------------------------------------------//
-//------------------------------------------------------------------------//
+	/**
+	 * Add column to posts management panel
+	 *
+	 */
+	function status_column( $columns ) {
+		$columns['lock_status'] = __( 'Lock Status', 'lock_posts' );
+		return $columns;
+	}
 
-function lock_posts_status_column($columns) {
-	$new_column = array('lock_status' => __('Lock Status'));
-	$columns = array_merge($columns, $new_column);
-	return $columns;
-}
+	/**
+	 * Display columns content on posts management panel
+	 *
+	 */
+	function status_output( $column, $id ) {
+		if( $column == 'lock_status' ) {
+			$post_lock_status = get_post_meta( $id, '_post_lock_status' );
 
-function lock_posts_meta_box() {
-	if ( is_site_admin() ) {
-		if ($_GET['action'] == 'edit'){
-			add_meta_box('postlock', __('Post Status'), 'lock_posts_meta_box_output', 'post', 'advanced', 'high');
-			add_meta_box('postlock', __('Post Status'), 'lock_posts_meta_box_output', 'page', 'advanced', 'high');
+			if( is_array( $post_lock_status ) && isset( $post_lock_status[0] ) )
+				$post_lock_status = $post_lock_status[0];
+
+			if( 'locked' == $post_lock_status )
+				echo __( 'Locked', 'lock_posts' );
+			else
+				echo __( 'Unlocked', 'lock_posts' );
 		}
 	}
-}
 
-function lock_posts_update($post_id) {
-	if ( !empty( $_POST['post_lock_status'] ) ) {
-		update_post_meta($post_id, '_post_lock_status', $_POST['post_lock_status']); 
+	/**
+	 * Add metabox to posts edition panel
+	 *
+	 */
+	function meta_box() {
+		if( is_super_admin() && !empty( $_GET['action'] ) && 'edit' == $_GET['action'] ) {
+			add_meta_box( 'postlock', __( 'Post Status', 'lock_posts' ), array( &$this, 'meta_box_output' ), 'post', 'advanced', 'high' );
+			add_meta_box( 'postlock', __( 'Post Status', 'lock_posts' ), array( &$this, 'meta_box_output' ), 'page', 'advanced', 'high' );
+		}
 	}
-}
 
-function lock_posts_check() {
-	if ( $_GET['action'] == 'edit' && !empty( $_GET['post'] ) && !is_site_admin() ) {
-		$post_lock_status = get_post_meta($_GET['post'], '_post_lock_status');
-		if ( is_array($post_lock_status) ) {
+	/**
+	 * Post status metabox
+	 *
+	 */
+	function meta_box_output( $post ) {
+		if ( !is_super_admin() )
+			return;
+
+		$post_lock_status = get_post_meta( $post->ID, '_post_lock_status' );
+		if( is_array( $post_lock_status ) && isset( $post_lock_status[0] ) )
 			$post_lock_status = $post_lock_status[0];
-		}
-		if ( $post_lock_status == 'locked' ) {
-			wp_redirect('locked.php');
+
+		if( empty( $post_lock_status ) )
+			$post_lock_status = 'unlocked';
+		?>
+		<div id="postlockstatus">
+			<label class="hidden" for="excerpt">Post Status</label>
+			<select name="post_lock_status">
+				<option value="locked" <?php selected( $post_lock_status, 'locked' ) ?>><?php _e( 'Locked', 'lock_posts' ) ?></option>
+				<option value="unlocked" <?php selected( $post_lock_status, 'unlocked' ) ?>><?php _e( 'Unlocked', 'lock_posts' ) ?></option>
+			</select>
+			<p><?php _e( 'Locked posts cannot be edited by anyone other than site admins.', 'lock_posts' ); ?></p>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Update post status
+	 *
+	 */
+	function update( $post_id ) {
+		if ( !empty( $_POST['post_lock_status'] ) && is_super_admin() )
+			update_post_meta( $post_id, '_post_lock_status', $_POST['post_lock_status'] );
+	}
+
+	/**
+	 * Check post status and redirect if the user is not super admin and post is locked
+	 *
+	 */
+	function check() {
+		if ( !is_super_admin() && !empty( $_GET['action'] ) && 'edit' == $_GET['action'] && !empty( $_GET['post'] ) ) {
+			$post_lock_status = get_post_meta( $_GET['post'], '_post_lock_status' );
+
+			if ( is_array($post_lock_status) )
+				$post_lock_status = $post_lock_status[0];
+
+			if ( $post_lock_status == 'locked' )
+				wp_redirect( admin_url( 'admin.php?page=post-locked&post=' . $_GET['post'] ) );
 		}
 	}
+
+	/**
+	 * Displayed 'locked' message
+	 *
+	 */
+	function locked() {
+		$post = get_post( $_GET['post'] );
+		echo '<div class="wrap">';
+		echo '<h2>' . __( 'Post Locked', 'lock_posts' ) . '</h2>';
+		echo '<p>' . sprintf( __( 'The post "%s" has been locked by a site administrator.', 'lock_posts' ), $post->post_title ) . '</p>';
+		echo '<p><a href="' . admin_url( 'edit.php?post_type=' . $post->post_type ) . '">&laquo; ' . __( 'Back to Posts List', 'lock_posts' ) . '</a></p>';
+		echo '</div>';
+	}
+
+	/**
+	 * Add admin page
+	 *
+	 */
+	function admin_page() {
+		global $menu;
+
+		$page = add_menu_page( 'Post Locked', 'Post Locked', 'edit_posts', 'post-locked', array( &$this, 'locked' ) );
+
+		foreach( $menu as $key => $menu_item ) {
+			if( isset( $menu_item[5] ) && $menu_item[5] == $page )
+				unset( $menu[$key] );
+		}
+	}
+
 }
 
-//------------------------------------------------------------------------//
-//---Output Functions-----------------------------------------------------//
-//------------------------------------------------------------------------//
-
-function lock_posts_status_output($column, $id) {
-	global $post;
-	if ( $column == 'lock_status' ) {
-		$post_lock_status = get_post_meta($id, '_post_lock_status');
-		if ( is_array($post_lock_status) ) {
-			$post_lock_status = $post_lock_status[0];
-		}
-		if ( $post_lock_status == 'locked' ) {
-			echo __('Locked');
-		} else {
-			echo __('Unlocked');
-		}
-	}
-}
-
-function lock_posts_meta_box_output($post) {
-	$post_lock_status = get_post_meta($post->ID, '_post_lock_status');
-	if ( is_array($post_lock_status) ) {
-		$post_lock_status = $post_lock_status[0];
-	}
-	if ( empty( $post_lock_status ) ) {
-		$post_lock_status = 'unlocked';
-	}
-	?>
-	<div id="postlockstatus">
-	<label class="hidden" for="excerpt">Post Status</label>
-	<select name="post_lock_status">
-	<option value="locked" <?php if ($post_lock_status == 'locked') echo 'selected="selected"'; ?>><?php _e('Locked') ?></option>
-	<option value="unlocked" <?php if ($post_lock_status == 'unlocked') echo 'selected="selected"'; ?>><?php _e('Unlocked') ?></option>
-	</select>
-	<p><?php _e('Locked posts cannot be edited by anyone other than site admins.'); ?></p>
-	</div>
-	<?php
-}
-
-
-//------------------------------------------------------------------------//
-//---Page Output Functions------------------------------------------------//
-//------------------------------------------------------------------------//
-
-function lock_posts_locked() {
-	if (isset($_GET['updated'])) {
-		?><div id="message" class="updated fade"><p><?php _e(urldecode($_GET['updatedmsg'])) ?></p></div><?php
-	}
-	echo '<div class="wrap">';
-	switch( $_GET[ 'action' ] ) {
-		//---------------------------------------------------//
-		default:
-			?>
-            <h2><?php _e('Post Locked') ?></h2>
-            <p><?php _e('This post has been locked by a site administrator.') ?></p>
-            <?php
-		break;
-		//---------------------------------------------------//
-	}
-	echo '</div>';
-}
-
-?>
+$lock_posts = new Lock_Posts();
